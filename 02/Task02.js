4e0bfdf696750aab0d4649a917427bf85c5b1a73
AnnaBuller <aniabela@wp.pl>
2021-02-08 17:47:03 +0100

    solve 02 task

--- 02/Task02.js
+++ 02/Task02.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Welcome from './../src/components/Welcome';
 import withPopup from './../src/hoc/withPopup';
 
-const ComponentWithPopup = withPopup(<Welcome/>);
+const ComponentWithPopup = withPopup('yellow', 0.6)(Welcome);
 
-const Task02 = () => (
-    <section>
-        <h2>Task 02</h2>
-        <ComponentWithPopup />
-    </section>
-)
+const Task02 = () => {
+    const [isPopupOpen, setPopupVisibility] = useState(true)
+
+	return (<section> 
+		<h2>Task 02</h2>
+		<ComponentWithPopup onClick={setPopupVisibility} visible={isPopupOpen} extraText = 'Udanego pobytu!'/>
+	</section>)
+};
 
 export default Task02;
 
--- src/hoc/withPopup.js
+++ src/hoc/withPopup.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
-const withPopup = () => {
-    return () => <section>withPopup()</section>;
-}
+const withPopup = (color, opacity) => (WrappedComponent) => {
+    const renderPopup = ({onClick, extraText}) => {
+        return (
+        <section style={{background: color, opacity: opacity, width: '300px', padding: '1rem', textAlign: 'center'}}>
+                <WrappedComponent>
+                </WrappedComponent>
+                <p>{extraText}</p>
+                <button onClick={() => onClick()}>ZAMKNIJ X</button>
+                </section>)
+    }
+
+    const renderWrappedComponent = () => {
+        return <WrappedComponent>
+        </WrappedComponent>
+    }
+    
+    return class extends React.Component {
+        render() {
+            const {visible} = this.props;
+            return visible ? renderPopup(this.props) : renderWrappedComponent();  
+        }
+    }
+};
 
 
 export default withPopup;
