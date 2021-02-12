b0bb281f101cbcbdcfc0fb8a2225fbf9b14c7f6a
Martyna Kiljan <kmartynak@icloud.com>
2021-02-12 19:04:12 +0100

    task 02 finish

--- src/hoc/withPopup.js
+++ src/hoc/withPopup.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {StyledModal, StyledCloseItem} from '../../02/StyledModal.styled'
 
-const withPopup = () => {
-    return () => <section>withPopup()</section>;
-}
+
+
+const withPopup = (
+    ModalColor = "#0000FF",
+    ModalOppacity = 0.7
+
+) =>  WrappedComponent => {
+        return props => {
+        
+        const [isOpenModal, setIsOpenModal] = useState(true);
+
+            return (
+             <>
+                <StyledModal isOpenModal={isOpenModal} >
+                  <StyledCloseItem onClick={() => setIsOpenModal(false)}>X</StyledCloseItem>
+                </StyledModal>
+
+              {!isOpenModal && <WrappedComponent {...props} />}
+             </>
+            )
+
+            
+        }
+
+ }
+
 
 
 export default withPopup;
