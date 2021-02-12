c5ebbc2faaba2397a96d1c2d0a50a86510f3d720
AnnaBuller <aniabela@wp.pl>
2021-02-12 14:23:23 +0100

    fix url and add proper parameters

--- src/modules/stackoverflow/stackoverflow.api.js
+++ src/modules/stackoverflow/stackoverflow.api.js
@@ -1,13 +1,18 @@
 export default class stackOverflowAPI {
 	url = 'https://api.stackexchange.com';
 
-	getResponse(sort = 'reputation') {
-		// 'creation'
-		return fetch(
-			`${this.url}/2.2/users?order=desc&sort=${sort}&site=stackoverflow`
-		)
-			.then(this.handleErrors)
-			.then((resp) => resp.json());
+	getResponse(query = 'how to ask questions', sortByDate = 'desc') {
+		const sortRule = sortByDate ? 'creation' : 'relevance';
+		const order = sortByDate;
+		return (
+			fetch(
+				
+				`${this.url}/2.2/similar?order=${order}&sort=${sortRule}&title=${query}&site=stackoverflow`
+			
+			)
+				.then(this.handleErrors)
+				.then((resp) => resp.json())
+		);
 	}
 
 	handleErrors(resp) {
