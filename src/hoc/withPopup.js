7a55452d32b2b49809997147eef692d842794b7d
AnnaBuller <aniabela@wp.pl>
2021-02-15 10:52:33 +0100

    remove unused closing tag

--- src/hoc/withPopup.js
+++ src/hoc/withPopup.js
@@ -3,12 +3,20 @@ import React from 'react';
 const withPopup = (color, opacity) => (WrappedComponent) => {
     const renderPopup = ({onClick, extraText}) => {
         return (
-        <section style={{background: color, opacity: opacity, width: '300px', padding: '1rem', textAlign: 'center'}}>
-                <WrappedComponent>
-                </WrappedComponent>
-                <p>{extraText}</p>
-                <button onClick={() => onClick()}>ZAMKNIJ X</button>
-                </section>)
+			<section
+				style={{
+					background: color,
+					opacity: opacity,
+					width: '300px',
+					padding: '1rem',
+					textAlign: 'center',
+				}}
+			>
+				<WrappedComponent />
+				<p>{extraText}</p>
+				<button onClick={() => onClick()}>ZAMKNIJ X</button>
+			</section>
+		);
     }
 
     const renderWrappedComponent = () => {
