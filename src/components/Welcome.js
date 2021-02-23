e411f1cb493aaa7fb3a485b49657ceae9b8310d5
mlvrkhn <martin.guide@gmail.com>
2021-02-23 21:12:15 +0100

    Connect Components with withPopup

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
-const Welcome = () => <h1>Witamy na naszej stronie!</h1>;
+const Welcome = (props) => {
+    console.log(props);
+    return <h1 style={{ ...props.style.style }}>Witamy na naszej stronie!</h1>;
+};
 
 export default Welcome;
-
--- src/hoc/withPopup.js
+++ src/hoc/withPopup.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
-const withPopup = () => {
-    return () => <section>withPopup()</section>;
-}
+const withPopup = (bgColor, opacity) => {
+    return function (WrappedComponent) {
+        return class Appearance extends React.Component {
+            state = {
+                backgroundColor: bgColor,
+                opacity: opacity,
+            };
 
+            render() {
+                const style = this.state;
+                return (
+                    <WrappedComponent style={{ style }}>ss</WrappedComponent>
+                );
+            }
+        };
+    };
+};
 
 export default withPopup;
