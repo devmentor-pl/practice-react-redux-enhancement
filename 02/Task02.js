233a2749f730829387256b9cf7cb2c6f8d5e0e69
mlvrkhn <martin.guide@gmail.com>
2021-02-24 01:38:53 +0100

    Pass props to button in withPopup

--- 02/Task02.js
+++ 02/Task02.js
@@ -2,14 +2,17 @@ import React, { useState } from 'react';
 import Welcome from './../src/components/Welcome';
 import withPopup from './../src/hoc/withPopup';
 
-const ComponentWithPopup = withPopup('yellow', 0.5)(Welcome);
+const ComponentWithPopup = withPopup('hotpink', 0.8)(Welcome);
 
 const Task02 = () => {
     const [isPopUpActive, setIsPopUpActive] = useState(false);
     return (
         <section>
             <h2>Task 02</h2>
-            <ComponentWithPopup />
+            <ComponentWithPopup
+                setIsPopUpActive={setIsPopUpActive}
+                isPopUpActive={isPopUpActive}
+            />
         </section>
     );
 };
--- src/hoc/withPopup.js
+++ src/hoc/withPopup.js
@@ -3,23 +3,28 @@ import React from 'react';
 const withPopup = (bgColor, opacity) => {
     return function (WrappedComponent) {
         return class Appearance extends React.Component {
-            state = {
-                backgroundColor: bgColor,
-                opacity: opacity,
-            };
-
             render() {
                 const styledPopup = {
-                    backgroundColor: 'green',
+                    backgroundColor: bgColor,
+                    opacity: opacity,
                     position: 'absolute',
+                    padding: '2rem',
                     left: '50%',
                     top: '50%',
                 };
+                const { isPopUpActive, setIsPopUpActive } = this.props;
+                console.log('🚀 ~ render ~ setIsPopUpActive', setIsPopUpActive);
+                console.log('🚀 ~ render ~ isPopUpActive', isPopUpActive);
 
                 return (
                     <div style={styledPopup}>
-                        <button style={{ float: 'right' }}>x</button>
-                        <WrappedComponent {...this.props} />
+                        <button
+                            onClick={() => togglePopUp()}
+                            style={{ float: 'right' }}
+                        >
+                            x
+                        </button>
+                        <WrappedComponent />
                     </div>
                 );
             }
