debdaa66cdea280947246fc1917117fa4de1e0e1
Maciej Zadylak <maciej.zadylak@gmail.com>
2021-12-13 15:28:49 +0100

    Task 03

---
+++ src/modules/github/github.reductor.js
@@ -0,0 +1,22 @@
+import types from './github.types';
+
+const INITIAL_STATE = {
+    repos: [],
+    errors: []
+};
+
+const reducer = (state = INITIAL_STATE, action) => {
+    switch(action.type) {
+        case types.GET_REPOS: 
+            return { ...state, repos: action.payload.value, errors: [] };
+        case types.FILTER_LIST:
+            const filteredList = state.repos.filter(el => el.name.includes(action.payload.value));
+            return { ...state, filteredRepos: [...filteredList] };
+        case types.HANDLE_ERRORS:
+            const { value, type } = action.payload;
+            return { ...state, errors: [ ...state.errors, { value: value, type: type }]}
+        default: return state;
+    };
+};
+
+export default reducer;
