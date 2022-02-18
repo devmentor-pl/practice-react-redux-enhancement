78bffd8c5613c68d0d12832d9226c9bce96d3924
Kowacka Malgorzata <kowackag@gmail.com>
2022-02-18 10:35:28 +0100

    Remove empty code

--- src/modules/stackoverflow/stack.action.js
+++ src/modules/stackoverflow/stack.action.js
@@ -4,7 +4,7 @@ import types from './stack.types';
 export const loadStack = payload => {
     return {
         type: types.LOAD_STACK,
-        payload: payload.items,
+        payload: payload.items
     }
 }
 
@@ -19,8 +19,4 @@ export const getStackAction = (title, sort) => (dispatch, getState) => {
     getStack(title, sort)
         .then(resp=>dispatch(loadStack(resp)))
         .catch(err=>dispatch(setError(err)))
-}
-
-export const getStackByVotesAction = (title) => (dispatch, useState) => {
-
 }
