c5043acdfc49b56fbdb7574ff1799bec540c29e9
Ewelina <ewelina@ekopacz.pl>
2022-05-12 16:38:33 +0200

    setinterval problem

--- 02/Task02.js
+++ 02/Task02.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import Welcome from './../src/components/Welcome';
 import withPopup from './../src/hoc/withPopup';
 
 const ComponentWithPopup = withPopup(Welcome);
 
-const Task02 = () => (
-    <section>
-        <h2>Task 02</h2>
-        <ComponentWithPopup />
-    </section>
-)
+const Task02 = () => {
+    const [isDisplay,setIsDisplay] = useState(false);
+    const [intervalId, setIntervalId] = useState(0);
+    console.log(intervalId);
+
+    useEffect(()=> {
+        const intervalId = setInterval(() => {
+            setIsDisplay(true)
+        }, 2000);
+        console.log(intervalId);
+        return ()=> clearInterval(intervalId);
+    },[])
+
+    const handleClick = () => {
+        console.log(intervalId);
+        setIsDisplay(false);
+        setIntervalId(0);
+        console.log(intervalId);
+    }
+
+
+    return (
+        <section>
+            <h2>Task 02</h2>
+            <ComponentWithPopup isDisplay={isDisplay} handleClick={handleClick}/>
+        </section>
+    )
+}
 
 export default Task02;
 
--- src/hoc/withPopup.js
+++ src/hoc/withPopup.js
@@ -1,8 +1,37 @@
 import React from 'react';
 
-const withPopup = () => {
-    return () => <section>withPopup()</section>;
+const boxStyles = {
+    border:'1px solid grey',
+    position:`fixed`,
+    top:`0`,
+    left:`0`,
+    width:`100%`,
+    height:`100vh`,
+    backgroundColor:`#fefbd8`,
 }
 
+const btnStyles = {
+    position:`absolute`,
+    right:`16px`,
+    top: `16px`,
+}
+
+
+const withPopup = (WrappedComponent) => {
+    return class PopUp extends React.Component {
+        render() {
+            const { isDisplay,handleClick } = this.props;
+            if(isDisplay) {
+                return (
+                    <div style={boxStyles}>
+                        <button style={btnStyles} onClick={handleClick}>X</button>
+                        <WrappedComponent {...this.props}/>
+                    </div>
+                )
+            }
+            return <div>Closed up</div>
+        }
+    }
+}
 
 export default withPopup;
