04443a56547b9252bfaf0ea3aa2f676b5bc3c40d
Filip <43039909+Urfisior@users.noreply.github.com>
2022-10-11 23:19:33 +0100

    task 02 done

--- 02/Task02.js
+++ 02/Task02.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import Welcome from './../src/components/Welcome';
 import withPopup from './../src/hoc/withPopup';
 
-const ComponentWithPopup = withPopup(Welcome);
+const ComponentWithPopup = withPopup('Siema')(Welcome);
 
-const Task02 = () => (
+const Task02 = () => {
+    const [popupIsOpen, setPopup] = useState(true);
+
+    const closePopup = () => {
+        return setPopup(!popupIsOpen);
+    };
+
+    return (
     <section>
         <h2>Task 02</h2>
-        <ComponentWithPopup />
+        <ComponentWithPopup popupIsOpen={popupIsOpen} closePopup={closePopup} />
     </section>
-)
+    )
+}
+
 
 export default Task02;
 
--- src/hoc/withPopup.js
+++ src/hoc/withPopup.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
-const withPopup = () => {
-    return () => <section>withPopup()</section>;
-}
+const withPopup = () => (WrappedContainer) => (props) => {
+    const { popupIsOpen, closePopup } = props;
+    if (popupIsOpen) {
+        return (
+            <div>
+                <WrappedContainer>
+                    <button onClick={()=>closePopup()}>Close</button>
+                </WrappedContainer>
+            </div>
+        )
+    }
+    return (
+        <WrappedContainer/>
+    )
 
+}
 
 export default withPopup;
