4ee65fc3dcc0f1e2adfac1877e8793f62e39ec0f
Filip <43039909+Urfisior@users.noreply.github.com>
2022-10-24 01:11:11 +0100

    task 04 done

--- src/modules/store.js
+++ src/modules/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import githubSlice from "./github/githubSlice";
+import stackoverflowSlice from "./stackoverflow/stackoverflowSlice";
 
 const store = configureStore({
     reducer: {
         github: githubSlice,
+        stackoverflow: stackoverflowSlice,
     }
 });
 
