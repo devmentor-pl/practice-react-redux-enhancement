ac94b45f0c1a4b43865c8a6602024a65e706ad0a
Andrej Kaczanowski <andykaczanowski@gmail.com>
2022-12-29 18:22:04 +0100

    Task 02

--- 02/Task02.js
+++ 02/Task02.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import Welcome from './../src/components/Welcome';
 import withPopup from './../src/hoc/withPopup';
+import { useState } from "react";
 
-const ComponentWithPopup = withPopup(Welcome);
+const ComponentWithPopup = withPopup("lime")(Welcome);
 
-const Task02 = () => (
-    <section>
-        <h2>Task 02</h2>
-        <ComponentWithPopup />
+const Task02 = () => {
+  const [isOpen, setIsOpen] = useState(true);
+  const overlayStyles = {
+    position: "fixed",
+    top: "0",
+    left: "0",
+    width: "100vw",
+    height: "100vh",
+    background: "rgba(0,0,0,0.7)",
+  };
+  const closePopup = () => {
+    return setIsOpen(!isOpen);
+  };
+  return (
+    <section style={isOpen ? overlayStyles : null}>
+      <h2 style={isOpen ? { display: "none" } : { display: "block" }}>
+        Task 02
+      </h2>
+      <ComponentWithPopup handleClick={closePopup} isOpen={isOpen} />
     </section>
-)
+  );
+};
 
 export default Task02;
 
--- src/hoc/withPopup.js
+++ src/hoc/withPopup.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { Component } from "react";
 
-const withPopup = () => {
-    return () => <section>withPopup()</section>;
-}
+const withPopup = (backgroundColor) => {
+  return function (WrappedComponent) {
+    const contentStyles = {
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
+      background: backgroundColor || "blue",
+      width: "500px",
+      height: "300px",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      border: "2px solid gray",
+      borderRadius: "20px",
+    };
 
+    return class extends Component {
+      render() {
+        const { isOpen, handleClick } = this.props;
+
+        if (isOpen) {
+          return (
+            <div style={contentStyles}>
+              <WrappedComponent {...this.props} />
+              <button onClick={handleClick}>X</button>
+            </div>
+          );
+        }
+        return <WrappedComponent />;
+      }
+    };
+  };
+};
 
 export default withPopup;
