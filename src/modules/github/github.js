775bbd7b67ebc241674b34dfc8ab4581e659dfa7
Andrej Kaczanowski <andykaczanowski@gmail.com>
2023-01-03 13:50:08 +0100

    Task 03 init

---
+++ src/modules/github/github.js
@@ -0,0 +1,8 @@
+import React from "react";
+import GitHubAPI from "./github.api";
+
+const Github = () => {
+  return <p>check</p>;
+};
+
+export default Github;
---
+++ src/modules/github/github.types.js
@@ -0,0 +1,7 @@
+const GET_REPOS = "getRepos";
+const FILTER_REPOS = "filterRepos";
+
+export default {
+  GET_REPOS,
+  FILTER_REPOS,
+};
