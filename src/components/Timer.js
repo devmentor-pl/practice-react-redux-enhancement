4a7fc03d2058bfcd7647ab61b4f883604638a7b6
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2023-02-02 13:34:25 +0100

    Add task1

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,41 +1,18 @@
 import React from 'react';
 
 class Timer extends React.Component {
-    state = {
-        time: 0,
-    }
-
-    getHours() {
-        return 0;
-    }
-
-    getMinutes() {
-        return 0;
-    }
-
-    getSeconds() {
-        return this.state.time;
-    }
+  
 
     render() {
-        return (
+    const {hours, minutes, seconds} = this.props.time
+    return (
             <section>
-                Dziękujemy, że jesteś z nami już {this.getHours()}h {this.getMinutes()}m {this.getSeconds()}s.
+                Dziękujemy, że jesteś z nami już {hours}h {minutes}m {seconds}s.
             </section>
         )
     }
 
-    componentDidMount() {
-        this.id = setInterval(() => {
-            this.setState(state => {
-                return {time: state.time + 1}
-            });
-        }, 1000);
-    }
 
-    componentWillUnmount() {
-        clearInterval(this.id);
-    }
 }
 
 export default Timer;
