022132225a62cb8aea5ed3d16a141637bc7b02e8
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2023-02-02 16:31:13 +0100

    Add task2

--- 02/Task02.js
+++ 02/Task02.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Welcome from './../src/components/Welcome';
 import withPopup from './../src/hoc/withPopup';
 
-const ComponentWithPopup = withPopup(Welcome);
+const ComponentWithPopup = withPopup(0.1, 20)(Welcome);
 
-const Task02 = () => (
-    <section>
-        <h2>Task 02</h2>
-        <ComponentWithPopup />
-    </section>
-)
+const Task02 = () => {
+    const [popUp, setPopUp] = useState(true)
+    return (
+        <section>
+            <h2>Task 02</h2>
+            <ComponentWithPopup active={popUp} onClose={setPopUp}/>
+        </section>
+    )
+
+}
+
 
 export default Task02;
 
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,5 +2,6 @@ import React from 'react';
 
 const Welcome = () => <h1>Witamy na naszej stronie!</h1>;
 
+
 export default Welcome;
 
