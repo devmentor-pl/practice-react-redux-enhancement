16cfbf7db5d9da8a87cba6d267f4be33a75d9323
Ewa Kulpinska <eva.kulpinska@gmail.com>
2023-07-23 16:03:52 +0200

    Corrected

--- 02/Task02.js
+++ 02/Task02.js
@@ -7,7 +7,7 @@ const ComponentWithPopup = withPopup({color: 'red'})(Welcome);
 const Task02 = () => (
     <section>
         <h2>Task 02</h2>
-        <ComponentWithPopup />
+        {/* <ComponentWithPopup /> */}
     </section>
 )
 
