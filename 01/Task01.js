17ac11a0ab86440cd9face23d9c1aaafaa21b76b
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-07-25 11:23:07 +0200

    Init

--- 01/Task01.js
+++ 01/Task01.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Timer from './../src/components/Timer';
 
 const Task01 = () => (
-    <section>
-        <h2>Task 01</h2>
-        <Timer/>
-    </section>
+	<>
+		<section>
+			<h2>Task 01</h2>
+			<Timer />
+		</section>
+	</>
 );
 
 export default Task01;
