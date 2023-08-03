1fe7195e6f566ea3e77051207a52d68defca17c4
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-08-03 09:14:24 +0200

    Add ex 1

---
+++ src/containers/TimerContainer.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Timer from "../components/Timer";
+
+class TimerContainer extends React.Component {
+	state = {
+		time: 0,
+	};
+
+	getHours() {
+		return Math.floor(this.state.time / 3600);
+	}
+
+	getMinutes() {
+		return Math.floor(this.state.time / 60);
+	}
+
+	getSeconds() {
+		return Math.floor(this.state.time % 60);
+	}
+
+	componentDidMount() {
+		this.id = setInterval(() => {
+			this.setState(state => {
+				return { time: state.time + 1 };
+			});
+		}, 1000);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.id);
+	}
+
+	render() {
+		return (
+			<Timer
+				hours={this.getHours()}
+				minutes={this.getMinutes()}
+				seconds={this.getSeconds()}
+			/>
+		);
+	}
+}
+
+export default TimerContainer;
