991cc352a58af08923746878b0406bbfa13926d9
kozerka <kozerka@gmail.com>
2023-10-16 21:24:29 +0200

    Task 04 done

--- src/app.js
+++ src/app.js
@@ -6,9 +6,11 @@ import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import App from './components/App';
 import reducer from "./modules/github/github.reducer";
+import stackoverflowReducer from "./modules/stackoverflow/stackoverflow.reducer";
 
 const rootReducer = combineReducers({
   github: reducer,
+  stackoverflow: stackoverflowReducer,
 });
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
