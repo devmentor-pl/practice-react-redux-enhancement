d869b90602f6aa97d6fefc437f24ff302f418888
Marcin <xcinek1337@wp.pl>
2023-11-14 21:51:43 +0100

    03

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import { githubReducer } from '../modules/github';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import Task01 from './../../01/Task01';
 import Task02 from './../../02/Task02';
@@ -9,7 +11,7 @@ import Task03 from './../../03/Task03';
 import Task04 from './../../04/Task04';
 import Task05 from './../../05/Task05';
 
-const store = createStore(githubReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(githubReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 const App = () => {
 	return (
--- src/modules/github/GithubComponent.js
+++ src/modules/github/GithubComponent.js
@@ -1,8 +1,60 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { GitHubAPI, setReposAction } from '../src/modules/github';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setReposAction, setErrorAction } from './github.actions';
+import GitHubAPI from './github.api';
 
 const GithubComponent = () => {
-	return console.log(`hi`);
+	const [searchValue, setSearchValue] = useState('');
+	const [filterReposValue, setFilterReposValue] = useState('');
+	const [loading, setLoading] = useState(false);
+	const { message, repos } = useSelector(state => state);
+	const newUser = new GitHubAPI();
+	const dispatch = useDispatch();
+
+	const fetchData = async userName => {
+		try {
+			setLoading(true);
+			const repos = await newUser.getRepos(userName);
+			const reposName = repos.map(repo => repo.name);
+			dispatch(setReposAction(reposName));
+		} catch (err) {
+			console.log(err);
+			dispatch(setErrorAction(err));
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		fetchData(searchValue);
+	};
+	const filteredRepos = filterReposValue
+		? repos.filter(repo => repo.toLowerCase().includes(filterReposValue.toLowerCase()))
+		: repos;
+
+	return (
+		<>
+			<form onSubmit={handleSubmit} action=''>
+				<input type='text' value={searchValue} onChange={e => setSearchValue(e.target.value)} />
+				<input type='submit' value={'search'} />
+			</form>
+			{loading && <p>ładuję...</p>}
+			{repos.length > 0 && (
+				<ul>
+					<input
+						placeholder='filter repos here...'
+						value={filterReposValue}
+						type='text'
+						onChange={e => setFilterReposValue(e.target.value)}
+					/>
+
+					{filteredRepos.map((repo, index) => (
+						<li key={index}>{repo}</li>
+					))}
+				</ul>
+			)}
+		</>
+	);
 };
 export default GithubComponent;
