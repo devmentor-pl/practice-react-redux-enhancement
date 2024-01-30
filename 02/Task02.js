c38f9df29c41b4e4e9f9c007a886bc9bcaaa297d
Kaczupinko <kaczupinko@gmail.com>
2024-01-30 17:25:51 +0000

    Task 02 done

--- 02/Task02.js
+++ 02/Task02.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Welcome from './../src/components/Welcome';
 import withPopup from './../src/hoc/withPopup';
 
 const ComponentWithPopup = withPopup(Welcome);
 
-const Task02 = () => (
-    <section>
-        <h2>Task 02</h2>
-        <ComponentWithPopup />
-    </section>
-)
+const Task02 = () => {
+    const [isPopupOpen, setIsPopupOpen] = useState(true);
+
+    const handleClosePopup = () => {
+        setIsPopupOpen(false);
+    };
+
+    return (
+        <section>
+            <h2>Task 02</h2>
+            {isPopupOpen ? (
+                <ComponentWithPopup handleClose={handleClosePopup} />
+            ) : (
+                <Welcome />
+            )}
+        </section>
+    );
+}
 
 export default Task02;
 
--- src/hoc/withPopup.js
+++ src/hoc/withPopup.js
@@ -1,8 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
-const withPopup = () => {
-    return () => <section>withPopup()</section>;
+
+const PopupOverlay = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(0, 0, 0, 0.5);
+`;
+
+const PopupWindow = styled.div`
+    background-color: white;
+    padding: 20px;
+    border-radius: 5px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
+    position: relative;
+    min-width: 300px;
+`;
+
+const CloseButton = styled.button`
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    background: none;
+    border: none;
+    font-size: 20px;
+    cursor: pointer;
+`;
+
+function withPopup(Component) {
+    return function WrappedComponent({ handleClose, ...props }) {
+        const [isOpen, setIsOpen] = useState(true);
+
+        const handleCloseClick = () => {
+            setIsOpen(false);
+            if (handleClose) {
+                handleClose();
+            }
+        };
+
+        return (
+            <>
+              {isOpen && (
+                <PopupOverlay>
+                  <PopupWindow>
+                    <CloseButton onClick={handleCloseClick}>X</CloseButton>
+                    <Component {...props} />
+                  </PopupWindow>
+                </PopupOverlay>
+              )}
+            </>
+          );
+    }
 }
-
 
 export default withPopup;
