a5b5345618e50cbcb7e150b7e2a85154f762ad8a
Kaczupinko <kaczupinko@gmail.com>
2024-01-30 16:32:16 +0000

    Finish 01 task

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,44 +1,9 @@
-import React from 'react';
+import React from "react";
 
-class Timer extends React.Component {
-    state = {
-        time: 0,
-    }
-
-    // Naprawiam godziny
-    getHours() {
-        return Math.floor(this.state.time / 3600);
-    }
-
-    // Naprawiam minuty
-    getMinutes() {
-        return Math.floor((this.state.time % 3600) / 60);
-    }
-
-    // Poprawiam sekundy
-    getSeconds() {
-        return this.state.time % 60;
-    }
-
-    render() {
-        return (
-            <section>
-                Dziękujemy, że jesteś z nami już {this.getHours()}h {this.getMinutes()}m {this.getSeconds()}s.
-            </section>
-        )
-    }
-
-    componentDidMount() {
-        this.id = setInterval(() => {
-            this.setState(state => {
-                return {time: state.time + 1}
-            });
-        }, 1000);
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.id);
-    }
-}
+const Timer = ({ hours, minutes, seconds }) => (
+    <section>
+        Dziękujemy, że jesteś z nami już {hours}h {minutes}m {seconds}s.
+    </section>
+);
 
 export default Timer;
