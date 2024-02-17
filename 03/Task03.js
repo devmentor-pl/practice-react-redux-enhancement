e316c634698075c65a5b17e15a2c0029ec80c06a
Michal Gawlik <gawlik.zator@o2.pl>
2024-02-17 15:17:56 +0100

    task 03 done

--- 03/Task03.js
+++ 03/Task03.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import GithubForm from '../src/modules/github/github'
 
 const Task03 = () => {
-
-
-    return (
-        <section>
-            <h2>Task 03</h2>
-        </section>
-    );
+	return (
+		<>
+			<section>
+				<h2>Task 03</h2>
+			</section>
+			<GithubForm />
+		</>
+	)
 }
 
 export default Task03;
