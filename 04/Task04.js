a2fa9c429cb8bd8df89f7a1b61e35903990ef03e
Michal Gawlik <gawlik.zator@o2.pl>
2024-02-18 15:11:28 +0100

    task 04 done

--- 04/Task04.js
+++ 04/Task04.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
+import StackOverflow from '../src/modules/stackoverflow/stackoverflow'
 
 const Task04 = () => {
-    return (
-        <section>
-            <h2>Task 04</h2>
-        </section>
-    );
+	return (
+		<section>
+			<h2>Task 04</h2>
+			<StackOverflow />
+		</section>
+	)
 }
 
 export default Task04;
